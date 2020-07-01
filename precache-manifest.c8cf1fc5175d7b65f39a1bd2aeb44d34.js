73d87106e5d92d49e3df192d16dd302f958c0a27
mucoo2762 <dmsruf2762@gmail.com>
2020-07-01 21:06:04 +0900

    Updates

---
+++ precache-manifest.c8cf1fc5175d7b65f39a1bd2aeb44d34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513645c40f2e730ec9dfe11d8729e1f2",
+    "url": "/Movie_app/index.html"
+  },
+  {
+    "revision": "d8a8e826e90e03cc365e",
+    "url": "/Movie_app/static/css/main.6c571b2a.chunk.css"
+  },
+  {
+    "revision": "134407de398ae4a3401c",
+    "url": "/Movie_app/static/js/2.d0edb342.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Movie_app/static/js/2.d0edb342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8a8e826e90e03cc365e",
+    "url": "/Movie_app/static/js/main.2f6e9d0c.chunk.js"
+  },
+  {
+    "revision": "e232abb5850372f70612",
+    "url": "/Movie_app/static/js/runtime-main.d910d05e.js"
+  }
+]);
